f0740cfb3a88d18b263cc3827a832976af209b83
Poornima <poornimamurali2803@gmail.com>
2023-11-21 13:00:09 +0530

    json server

---
+++ index.js
@@ -0,0 +1,16 @@
+const jsonServer = require('json-server')
+
+const mediaPlayerServer = jsonServer.create()
+
+const router = jsonServer.router("db.json")
+
+const middlewares = jsonServer.defaults()
+
+const port = 4000 || process.env.PORT
+
+mediaPlayerServer.use(middlewares)
+mediaPlayerServer.use(router)
+
+mediaPlayerServer.listen(port,()=>{
+    console.log(`Media Player Server started at port ${port}, and waiting for client request!!!`);
+})
